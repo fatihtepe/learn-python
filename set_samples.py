397c647f1f7c5b31558699b7025df9ec777b2c07
fatihtepe <fatihtepe@gmail.com>
2021-04-11 21:58:49 -0400

    set samples

--- set_samples.py
+++ set_samples.py
@@ -2,17 +2,17 @@
 # empty_set = set()
 # print(type(empty_set))
 
-stRing_sample = "elmaarmutportakalordakal"
-yset = set(stRing_sample)
-print(yset) 
+# stRing_sample = "elmaarmutportakalordakal"
+# yset = set(stRing_sample)
+# print(yset) 
  
-list_sampel = city = ['New York', 'London', 'Seoul', 'Sydney', 111, 222]
-xset = set(list_sampel)
-print(xset)
+# list_sampel = city = ['New York', 'London', 'Seoul', 'Sydney', 111, 222]
+# xset = set(list_sampel)
+# print(xset)
 
-tuple_sample = (True, False, 1, 0, "a", "b", None)
-zset = set(tuple_sample)
-print(zset) #  True = 1 False = 0 REMEMBER!! 
+# tuple_sample = (True, False, 1, 0, "a", "b", None)
+# zset = set(tuple_sample)
+# print(zset) #  True = 1 False = 0 REMEMBER!! 
 
 state_capitals = {'Ontario': 'Toronto',
                    'Alberta': 'Edmonton',
@@ -28,11 +28,11 @@ state_capitals = {'Ontario': 'Toronto',
                    'Yukon': 'Whitehorse',
                    }
 
-set_from_dict = set(state_capitals)
-print(set_from_dict)
+# set_from_dict = set(state_capitals)
+# print(set_from_dict)
 
-set_from_values = set(state_capitals.values())
-print(set_from_values)
+# set_from_values = set(state_capitals.values())
+# print(set_from_values)
 
 set_from_items = set(state_capitals.items())
 print(set_from_items)
