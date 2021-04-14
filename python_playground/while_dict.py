f937ab4ebbe50d6367a5b9c35d235ec6437e0096
fatihtepe <fatihtepe@gmail.com>
2021-04-14 01:02:42 -0400

    dictionary while

---
+++ python_playground/while_dict.py
@@ -0,0 +1,17 @@
+# a = 1
+
+# while a < 10:
+#     a += 1
+#     print('Be a warrior not a worrior!')
+
+# print(*enumerate("istihza"))
+
+# print(*reversed("istihza"))
+
+eng_tur_dict = {"dil": "language", "bilgisayar": "computer", "masa": "table"}
+
+anlam = input("Lütfen anlamını öğrenmek istediğiniz kelimeyi yazınız:")
+
+
+
+print(eng_tur_dict.get(anlam, 'Bu kelime veritabanimizda yoktur! '))
